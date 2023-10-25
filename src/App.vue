a6e917efd8e9dade4b4f6a20050c2ffdb10ceb4a
Fatemeh Ebrahimzadeh <F.Ebrahimzadeh@Alibaba.ir>
2023-10-25 11:32:03 +0330

    feat(): add main layout to project

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,31 @@
-<script setup lang="ts">
+<script lang="ts">
 import { RouterView } from 'vue-router'
+import TheHeader from '@/components/layout/TheHeader.vue'
+
+export default {
+  components: {
+    TheHeader
+  }
+}
 </script>
 
 <template>
-  <RouterView />
+  <the-header />
+  <router-view />
 </template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
