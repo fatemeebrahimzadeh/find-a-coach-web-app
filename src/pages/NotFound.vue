2e7a41ce5f2c486e97cc0bdb99b2582de451509f
Fatemeh Ebrahimzadeh <F.Ebrahimzadeh@Alibaba.ir>
2023-11-02 17:42:02 +0330

    feat(): add not found page

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,3 +1,11 @@
 <template>
-  <div>Page Note Found!</div>
+  <section>
+    <base-card>
+      <h2>Page not found</h2>
+      <p>
+        This page could not be found - Maybe check out all our
+        <router-link :to="{ name: 'coaches' }">coaches</router-link>.
+      </p>
+    </base-card>
+  </section>
 </template>
